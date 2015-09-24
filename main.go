8e08f5802a2c1ca8d156a02da5f11ff20dea3417
Mark St.Godard <markstgodard@gmail.com>
2015-09-23 20:52:59 -0500

    Rewrite in Go, first cut

--- main.go
+++ main.go
@@ -1,23 +1,89 @@
 package main
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"sort"
+	"strconv"
 	"time"
 
 	"github.com/tedsuo/rata"
 )
 
-func newIndexHandler() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "index")
-	})
+const viewsDir = "public"
+const reportDir = "./logs"
+
+type TestCases []TestCase
+
+type TestSuite struct {
+	XMLName   xml.Name   `xml:"testsuite"`
+	Tests     string     `xml:"tests,attr"`
+	Time      string     `xml:"time,attr"`
+	TestCases []TestCase `xml:"testcase"`
+}
+type TestCase struct {
+	Name      string `xml:"name,attr" json:"name"`
+	Time      string `xml:"time,attr" json:"time"`
+	Classname string `xml:"classname,attr" json:"package"`
+	Failure   string `xml:"failure"`
+	Result    string `json:"result"`
+}
+
+type testSummary struct {
+	TotalPassed int       `json:"total_passed"`
+	TotalFailed int       `json:"total_failed"`
+	TotalTime   float32   `json:"total_time"`
+	Results     TestCases `json:"results"`
+}
+
+type byModTime []os.FileInfo
+
+func (f byModTime) Len() int           { return len(f) }
+func (f byModTime) Less(i, j int) bool { return f[i].ModTime().Unix() < f[j].ModTime().Unix() }
+func (f byModTime) Swap(i, j int)      { f[i], f[j] = f[j], f[i] }
+
+func collectResults() testSummary {
+	t := testSummary{}
+
+	files, _ := ioutil.ReadDir(reportDir)
+	sort.Sort(byModTime(files))
+	for _, f := range files {
+		xmlFile, err := ioutil.ReadFile(reportDir + "/" + f.Name())
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			break
+		}
+
+		ts := TestSuite{}
+		xml.Unmarshal(xmlFile, &ts)
+
+		for _, tc := range ts.TestCases {
+			if tc.Failure != "" {
+				tc.Result = "failed"
+				t.TotalFailed++
+			} else {
+				tc.Result = "pass"
+				t.TotalPassed++
+			}
+
+			f, _ := strconv.ParseFloat(tc.Time, 32)
+			t.TotalTime += float32(f)
+
+			t.Results = append(t.Results, tc)
+
+		}
+	}
+	return t
 }
 
 func newTestsHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "tests")
+		json.NewEncoder(w).Encode(collectResults())
 	})
 }
 
@@ -29,7 +95,7 @@ func main() {
 	}
 
 	handlers := map[string]http.Handler{
-		"get_index": newIndexHandler(),
+		"get_index": http.FileServer(http.Dir(viewsDir)),
 		"get_tests": newTestsHandler(),
 	}
 
@@ -47,5 +113,4 @@ func main() {
 	}
 
 	log.Fatal(server.ListenAndServe())
-
 }
